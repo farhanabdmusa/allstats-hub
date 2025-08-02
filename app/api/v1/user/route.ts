162553d81622515fc469d83738f0d48e048f98f5
Farhan Abdurrahman Musa <farhan.musa@bps.go.id>
2025-08-02 13:02:15 +0700

    feat(user): change user method only PUT to update user

--- app/api/v1/user/route.ts
+++ app/api/v1/user/route.ts
@@ -1,98 +1,75 @@
 import { type NextRequest } from "next/server";
 import { ipAddress } from '@vercel/functions'
 import prisma from "@/lib/prisma";
-import { CreateUserPayload } from '@/types/user';
 import createApiResponse from "@/lib/create_api_response";
-import { convertDate2String, getCurrentDateTime } from "@/lib/jakarta_datetime";
+import { convertDate2String } from "@/lib/jakarta_datetime";
 import z from "zod";
+import { jwtVerify } from "jose";
+import { AUDIENCE, SECRET_KEY } from "@/constants/v1/api";
+import UserSchema from "@/zod/user_schema";
 
-const UserSchema = z.object({
-    uuid: z.string("UUID is required"),
-    email: z.email().optional(),
-    manufacturer: z.string().optional(),
-    device_model: z.string().optional(),
-    os: z.string().optional(),
-    os_version: z.string().optional(),
-    is_virtual: z.boolean().optional(),
-    last_ip: z.ipv4().optional(),
-});
-
-export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams;
-    const id = Number(searchParams.get("id"));
-    if (!id || Number.isNaN(id)) {
-        return createApiResponse({
-            status: false,
-            message: "Invalid or missing 'id' parameter",
-            statusCode: 422,
-        });
-    }
-
-
+export async function PUT(request: NextRequest) {
     try {
-        const user = await prisma.user.findUniqueOrThrow({
-            where: { id },
-            include: {
-                sign_up_type_relation: true,
+        const authHeader = request.headers.get('authorization');
+        const token = authHeader?.split(' ')[1];
+        const jwt = await jwtVerify(
+            token!,
+            SECRET_KEY,
+            { audience: AUDIENCE }
+        );
+
+        const check = await prisma.user.findUnique({
+            select: {
+                id: true
+            },
+            where: {
+                id: Number(jwt.payload.sub!)
             }
         })
-        const result = {
-            ...user,
-            first_session: convertDate2String(user.first_session),
-            last_session: convertDate2String(user.last_session)
+        if (!check) {
+            return createApiResponse({
+                status: false,
+                message: "User not found",
+                statusCode: 404
+            })
         }
+
+        const formData = await request.json();
+
+        const validatedData = UserSchema.safeParse({
+            ...formData,
+            user_id: check.id
+        })
+        if (!validatedData.success) {
+            const errors = z.treeifyError(validatedData.error);
+            return createApiResponse({
+                status: false,
+                zodError: errors,
+            })
+        }
+        const last_ip = ipAddress(request);
+
+        const user = await prisma.user.update({
+            where: {
+                id: check.id
+            },
+            data: { ...validatedData.data, last_ip }
+        })
 
         return createApiResponse({
             status: true,
-            data: result,
+            data: {
+                ...user,
+                first_session: convertDate2String(user.first_session),
+                last_session: convertDate2String(user.last_session),
+            },
         })
     } catch (error) {
-        console.log("🚀 ~ GET /api/v1/user:", error)
+        console.log("🚀 ~ PUT /api/v1/user ~ error:", error)
         return createApiResponse({
             status: false,
             message: "Internal Server Error",
             statusCode: 500,
         })
     }
-}
-
-export async function POST(request: NextRequest) {
-    const formData = await request.json() as CreateUserPayload;
-    const last_ip = ipAddress(request);
-    formData["last_ip"] = last_ip;
-
-    const validatedData = UserSchema.safeParse(formData);
-
-    if (!validatedData.success) {
-        const errors = z.treeifyError(validatedData.error);
-        return createApiResponse({
-            status: false,
-            zodError: errors,
-        })
-    }
-
-    try {
-        await prisma.user.upsert({
-            create: formData,
-            update: {
-                ...formData,
-                last_session: getCurrentDateTime()!
-            },
-            where: {
-                uuid: formData.uuid
-            }
-        })
-
-        return createApiResponse({
-            status: true,
-        });
-    } catch (error) {
-        console.log("🚀 ~ POST /api/v1/user:", error)
-        return createApiResponse({
-            status: false,
-            message: "Failed to insert user",
-            statusCode: 500,
-        })
-    }
-
 }
