20bc33d41351bf1857c3c413840f6cf41e9b3034
Farhan Abdurrahman Musa <farhan.musa@bps.go.id>
2025-09-04 11:19:42 +0700

    feat(sidebar): improve sidebar menu item highlighting based on current path

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -10,6 +10,7 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function NavMain({
   items,
@@ -20,12 +21,17 @@ export function NavMain({
     icon?: Icon;
   }[];
 }) {
+  const pathname = usePathname();
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
+            <SidebarMenuItem
+              key={item.title}
+              className={pathname === item.url ? "bg-sky-700/5 rounded-md" : ""}
+            >
               <SidebarMenuButton tooltip={item.title} asChild>
                 <Link href={item.url}>
                   {item.icon && <item.icon />}
