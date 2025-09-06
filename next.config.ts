88350d8d0a525eb13479682cf90b6380690f2125
Farhan Abdurrahman Musa <farhan.musa@bps.go.id>
2025-09-06 10:41:24 +0700

    feat(config): add allowedDevOrigins for local development environments

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '192.168.0.220', 'localhost'],
 };
 
 export default nextConfig;
