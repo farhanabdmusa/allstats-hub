8ddc8ada721b954c1f493eefe22ba23debc6ab1c
Farhan Abdurrahman Musa <famusa98@gmail.com>
2025-09-09 13:08:44 +0700

    fix(constant): update APP_KEY to use ALLSTATS_SECRET_KEY environment variable

--- constants/v1/api.ts
+++ constants/v1/api.ts
@@ -1,5 +1,5 @@
 export const ALLOWED_ORIGIN = "Allstats BPS";
-export const APP_KEY = process.env.ALLSTATS_APP_KEY;
+export const APP_KEY = process.env.ALLSTATS_SECRET_KEY;
 // JWT
 export const SECRET_KEY = new TextEncoder().encode(process.env.SIGNATURE_SECRET_KEY);
 export const ISSUER = "https://bps.go.id";
