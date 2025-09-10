f249b026a7f87415eb88903442d54ae198d742a5
Farhan Abdurrahman Musa <famusa98@gmail.com>
2025-09-10 22:42:55 +0800

    fix(topic): refine query structure to include user_select filter

--- app/api/v1/topic/route.ts
+++ app/api/v1/topic/route.ts
@@ -2,14 +2,18 @@ import createApiResponse from "@/lib/create_api_response";
 import prisma from "@/lib/prisma";
 
 export async function GET() {
+    // TODO: consider caching if the topics are not frequently changed
     try {
         const result = await prisma.topic.findMany({
             orderBy: {
                 id: "asc"
             },
+            where: {
+                // Only include topics that are selectable by users
+                user_select: true,
+                // TODO: consider limiting fields by createdAt or updatedAt
+            }
             // TODO: consider pagination if the number of topics grows large
-            // TODO: consider limiting fields by createdAt or updatedAt
-            // TODO: consider caching if the topics are not frequently changed
         });
 
         return createApiResponse({
